b86eb78e86e36e438ef6f1ff14b6c4380ce0da03
バントウフウマ <coolishpoo3@gmail.com>
2021-03-13 16:10:04 +0900

    add kinetic component

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,27 @@
       <h3>{{totalNumber}}</h3>
     </LikeHeader>
     <LikeNumber :totalNumber="totalNumber" @my-click="incrementNumber"></LikeNumber>
-    <LikeNumber :totalNumber="totalNumber" @my-click="incrementNumber"></LikeNumber>
+    <button @click="currentComponent = 'Home' ">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <component :is="currentComponent"></component>
   </div>
 </template>
 
 <script>
 import LikeHeader from './components/LikeHeader.vue'
+import About from './components/About'
+import Home from './components/Home'
 
 export default{
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home
   },
   data(){
     return {
-      totalNumber: 8
+      totalNumber: 8,
+      currentComponent: 'Home'
     }
   },
   methods: {
